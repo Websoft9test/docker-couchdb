04cfb93c3d6f6a4c58c319cab7c21053541ef269
qiaofeng1227 <76487013@qq.com>
2022-07-19 17:35:29 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "couchdb version:" $(cat /data/wwwroot/couchdb/releases/*/couchdb.rel  |sed -n 3p | awk -F '"' '{print $4}') |sudo tee -a /data/logs/install_version.txt
+sudo echo "couchdb version:" $(docker exec -i $1 cat /opt/couchdb/releases/RELEASES  |sed -n 2p | awk -F '"' '{print $4}') |sudo tee -a /data/logs/install_version.txt
